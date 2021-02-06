196e7f3bc48acbcd288574e808b583e7ba69c791
Maxim <yugov.max@mail.ru>
2021-02-06 20:39:57 +0700

    Starting api implementation

---
+++ API/CoaApplication.cs
@@ -0,0 +1,13 @@
+﻿using CoaApp.Core.Model;
+using System;
+
+namespace API
+{
+    public class CoaApplication : Application
+    {
+        public CoaApplication() : base()
+        {
+
+        }
+    }
+}
---
+++ API/CoaSession.cs
@@ -0,0 +1,12 @@
+﻿using CoaApp.Core.Model;
+
+namespace API
+{
+    public class CoaSession : Session
+    {
+        internal CoaSession(CoaApplication app): base(app)
+        {
+
+        }
+    }
+}
