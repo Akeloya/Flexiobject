4352ba549b185bef081c4af44307930ace35438c
Maxim <yugov.max@mail.ru>
2021-02-06 20:40:24 +0700

    Start server side implementation

---
+++ AppServer/Model/CoaApplication.cs
@@ -0,0 +1,15 @@
+﻿using CoaApp.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppServer.Model
+{
+    internal class CoaApplication: Application
+    {
+        internal CoaApplication() : base()
+        {
+
+        }
+    }
+}
