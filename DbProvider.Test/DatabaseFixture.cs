567a4fcc862960f45732590426f981d13b303b51
Maxim <yugov.max@mail.ru>
2021-02-14 19:30:40 +0700

    db connection manager for all tests

---
+++ DbProvider.Test/DatabaseFixture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DbProvider.Test
+{
+    public class DatabaseFixture : IDisposable
+    {
+        AppDbSettings _settings = new AppDbSettings(DbTypes.SqlLight, "localhost", "TestDb", null, null);
+
+        public AppDbSettings Settings => _settings;
+        public DatabaseFixture()
+        {
+            SqlManager.RegisterManager(_settings);
+        }
+        public void Dispose()
+        {
+            
+        }
+    }
+}
