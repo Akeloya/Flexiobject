eca2a52c7f32d6f648e9fc761030b1b79a15dbfb
Maxim <yugov.max@mail.ru>
2021-02-16 23:31:57 +0700

    Add name generating helper

---
+++ DbProvider.Test/TestHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DbProvider.Test
+{
+    static class TestHelpers
+    {
+        public static string GetRandName()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+    }
+}
