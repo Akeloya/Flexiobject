2b743e5d23e16d50156dddfc2818569095653fd3
Maxim <yugov.max@mail.ru>
2021-02-23 23:42:46 +0700

    Add passing test to dbProvider

--- DbProvider.Test/InitiateDb.cs
+++ DbProvider.Test/InitiateDb.cs
@@ -1,15 +1,16 @@
 using System;
+using System.Linq;
 using Xunit;
 
 namespace DbProvider.Test
 {
-    public class InitiateDb : IDisposable
+    public class InitiateDb : IClassFixture<DatabaseFixture>
     {
-        AppDbSettings _settings = new AppDbSettings(DbTypes.SqlLight, "localhost", "TestDb",null,null);
+        DatabaseFixture _fixture;
 
-        public InitiateDb()
+        public InitiateDb(DatabaseFixture fixture)
         {
-            SqlManager.RegisterManager(_settings);
+            _fixture = fixture;
         }
         [Fact]
         public void OpenConnection()
@@ -17,21 +18,51 @@ namespace DbProvider.Test
             using ISqlConnectionItem connection = SqlManager.Register(Guid.NewGuid());
             connection.BeginTransaction();
             var ctx = connection.Context;
-            ctx.ObjectReport.Add(new Entities.ObjectDef
+
+            int objId = 1;
+
+            for(int i = 0; i < 10; i++)
             {
-                Id = 1,
-                Created = DateTime.Now,
-                FolderId = 0,
-                UserDeletedById = 0
-            });
+                int folderId = i + 1;
+                ctx.ObjectFolders.Add(new Entities.ObjectFolder
+                {
+                    Name = TestHelpers.GetRandName(),
+                    Alias = TestHelpers.GetRandName(),
+                    ParentId = int.MaxValue,
+                    Id = folderId,
+                });
+                for(int j = 0; j < 100; j++)
+                {
+                    ctx.ObjectReport.Add(new Entities.ObjectDef
+                    {
+                        Id = objId++,
+                        Created = DateTime.Now,
+                        FolderId = folderId,
+                        UserDeletedById = 0
+                    });
+                }
+                ctx.SaveChanges();
+            }
+            
             ctx.SaveChanges();
+
+            int countFolders = ctx.ObjectFolders.Count();
+            Assert.Equal(10, countFolders);
+
+            int countObj = ctx.ObjectReport.Count();
+            Assert.Equal(1000, countObj);
+
+            ctx.ObjectFolders.Remove(ctx.ObjectFolders.Where(x => x.Id == 1).Single());
+            ctx.SaveChanges();
+
+            countFolders = ctx.ObjectFolders.Count();
+            Assert.Equal(9, countFolders);
+
+            countObj = ctx.ObjectReport.Count();
+            Assert.Equal(900, countObj);
+
             connection.CommitTransaction();
             connection.Close();
-        }
-
-        public void Dispose()
-        {
-            
         }
     }
 }
