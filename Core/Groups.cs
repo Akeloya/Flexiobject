b150f091af513f4af23397a0bba05c65cb76da9a
Maxim <yugov.max@mail.ru>
2021-07-09 00:07:07 +0700

    groups base class implementation

---
+++ Core/Groups.cs
@@ -0,0 +1,39 @@
+﻿/*
+ *  "Custom object application core"
+ *  Application for creating and using freely customizable configuration of data, forms, actions and other things
+ *  Copyright (C) 2018 by Maxim V. Yugov.
+ *
+ *  This file is part of "Custom object application".
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using CoaApp.Core.Interfaces;
+
+namespace CoaApp.Core
+{
+    public abstract class Groups : AppBase, IGroups
+    {
+        protected Groups(IApplication app, object parent): base(app, parent)
+        {
+
+        }
+        public abstract IGroup this[int index] { get; }
+        public abstract IGroup this[string name] { get; }
+        public abstract int Count { get; }
+        public abstract IGroup Add();
+        public abstract void Remove(IGroup obj);
+        public abstract void Remove(int index);
+    }
+}
