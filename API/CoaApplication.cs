e90e0171ccbe36653b28612ae33b222afbd08faa
Maxim <yugov.max@mail.ru>
2021-07-09 18:00:37 +0700

    fixes to build solution

--- API/CoaApplication.cs
+++ API/CoaApplication.cs
@@ -1,13 +1,29 @@
 ﻿using CoaApp.Core;
+using CoaApp.Core.Interfaces;
 using System;
 
 namespace API
 {
-    public class CoaApplication : Application
+    public class ClntApplication : CoaApplication
     {
-        public CoaApplication() : base()
+        public ClntApplication()
         {
 
+        }
+
+        protected override void OnLogMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ISession OnOpenSession(string host, int port)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override ISession OnOpenSessionWithLoginPassword(string host, int port, string login, string password)
+        {
+            throw new NotImplementedException();
         }
     }
 }
